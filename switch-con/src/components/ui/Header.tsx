8d7eae146d2bc0bafab58063e6070c35a5289971
Jo Seung Hyo <seunghyo7742@naver.com>
2023-11-26 16:20:42 +0900

    ✨ Feat: header 뒤로가기버튼 추가

--- switch-con/src/components/ui/Header.tsx
+++ switch-con/src/components/ui/Header.tsx
@@ -1,5 +1,5 @@
 import { SlArrowLeft } from 'react-icons/sl';
-
+import { useNavigate } from 'react-router-dom';
 interface HeaderProps {
 	headline: string;
 	navigaterOff?: boolean;
@@ -7,11 +7,21 @@ interface HeaderProps {
 }
 
 const Header = ({ headline, navigaterOff, children }: HeaderProps) => {
+	const navigate = useNavigate();
 	return (
 		<>
 			<header className='fixed bg-white w-[375px] shadow-sm z-50'>
 				<div className={`flex flex-row items-center ${navigaterOff ? 'justify-between' : ''} w-full py-3 pl-2`}>
-					{!navigaterOff && <SlArrowLeft size='20' />}
+					{!navigaterOff && (
+						<button
+							onClick={() => {
+								navigate(-1);
+							}}
+						>
+							<SlArrowLeft size='20' />
+						</button>
+					)}
+
 					<div className='mx-2 text-lg font-bold'>{headline}</div>
 					<div className='flex'>{children}</div>
 				</div>
