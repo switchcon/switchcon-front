c1da0bb1604402f436180d72e00301f36869d52f
jeon-yuchan <69443437+jeon-yuchan@users.noreply.github.com>
2023-11-30 00:56:04 +0900

    post, get, delete

--- switch-con/src/apis/authAxios.tsx
+++ switch-con/src/apis/authAxios.tsx
@@ -3,7 +3,7 @@ import { getNewRefreshToken } from './refresh';
 
 export const getAuthAxios = (token) => {
 	const authAxios = axios.create({
-		baseURL: 'http://3.36.253.248:8080/login',
+		baseURL: 'http://3.36.253.248:8080',
 		headers: {
 			Authorization: token,
 		},
---
+++ switch-con/src/get/gifticonAll.tsx
@@ -0,0 +1,24 @@
+import { getHome } from '@apis/getHome';
+import axios from 'axios';
+
+export const gifticonAll = async (sortType: number) => {
+	const authToken = getHome();
+
+	try {
+		const result = await axios.get(
+			`http://3.36.253.248:8080/gifticon/all/${sortType}`, // Use template literal to include gifticonId in the URL
+			{
+				headers: {
+					Authorization: `Bearer ${authToken}`,
+					'Content-Type': 'application/json',
+				},
+			},
+		);
+
+		return result.data;
+	} catch (error) {
+		// Handle error
+		console.error('Error during DELETE request:', error);
+		throw error; // Re-throw the error if needed
+	}
+};
