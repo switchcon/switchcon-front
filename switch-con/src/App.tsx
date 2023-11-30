905a6a9cc45b46bba2b168321b7c6eaf4fbce725
Jo Seung Hyo <seunghyo7742@naver.com>
2023-12-01 01:29:11 +0900

    ✨ Feat: 기프티콘 등록페이지 퍼블리싱

--- switch-con/src/App.tsx
+++ switch-con/src/App.tsx
@@ -2,6 +2,7 @@ import ExchangeHome from '@pages/exchange/ExchangeHome';
 import ExchangePostCreate from '@pages/exchange/ExchangePostCreate';
 import ExchangePostDetail from '@pages/exchange/ExchangePostDetail';
 import ExchangeRequest from '@pages/exchange/ExchangeRequest';
+import GiftConRegisterPage from '@pages/giftCon/GiftConRegister';
 import Home from '@pages/mainPage/Home';
 import Landing from '@pages/mainPage/Landing';
 import MyExchange from '@pages/myExchange/MyExchange';
@@ -21,7 +22,8 @@ const App = () => {
 			<Route path='/exchange-request/:id' element={<ExchangeRequest />} />
 			{/*교환 요청페이지. 기프티콘 상세에서 교환신청 버튼으로 이동. id값: 기프티콘 게시글 id */}
 			<Route path='/exchange-post' element={<ExchangePostCreate />} /> {/*교환 게시글 작성 */}
-			<Route path='/my-con' element={<MyExchange />} />
+			<Route path='/my-con' element={<MyExchange />} /> {/*내콘 화면 */}
+			<Route path='/home/giftcon-regi' element={<GiftConRegisterPage />} /> {/* 기프티콘 등록페이지 */}
 		</Routes>
 	);
 };
