a8d8ab7339ac729a9614062d0b605420173e584a
Jo Seung Hyo <seunghyo7742@naver.com>
2023-12-03 16:55:29 +0900

    ✨ Feat: 기프티콘 조회페이지 사용처 이외 퍼블리싱 완료

--- switch-con/src/App.tsx
+++ switch-con/src/App.tsx
@@ -2,7 +2,8 @@ import ExchangeHome from '@pages/exchange/ExchangeHome';
 import ExchangePostCreate from '@pages/exchange/ExchangePostCreate';
 import ExchangePostDetail from '@pages/exchange/ExchangePostDetail';
 import ExchangeRequest from '@pages/exchange/ExchangeRequest';
-import GiftConRegisterPage from '@pages/giftCon/GiftConRegister';
+import GiftconPost from '@pages/giftcon/GiftconPost';
+import GiftConRegisterPage from '@pages/giftcon/GiftconRegister';
 import Home from '@pages/mainPage/Home';
 import Landing from '@pages/mainPage/Landing';
 import MyExchange from '@pages/myExchange/MyExchange';
@@ -11,10 +12,12 @@ import { Route, Routes } from 'react-router-dom';
 const App = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Landing />} />
 			{/*랜딩페이지*/}
-			<Route path='/home' element={<Home />} />
+			<Route path='/' element={<Landing />} />
 			{/*홈- 내가 등록한 기프티콘 보는 곳 */}
+			<Route path='/home' element={<Home />} />
+			<Route path='/home/giftcon-regi' element={<GiftConRegisterPage />} /> {/* 기프티콘 등록페이지 */}
+			<Route path='/home/giftcon/:id' element={<GiftconPost />} /> 기프티콘 상세페이지
 			<Route path='/exchange-home' element={<ExchangeHome />} />
 			{/*교환 홈 */}
 			<Route path='/exchange/:id' element={<ExchangePostDetail />} />
@@ -23,7 +26,6 @@ const App = () => {
 			{/*교환 요청페이지. 기프티콘 상세에서 교환신청 버튼으로 이동. id값: 기프티콘 게시글 id */}
 			<Route path='/exchange-post' element={<ExchangePostCreate />} /> {/*교환 게시글 작성 */}
 			<Route path='/my-con' element={<MyExchange />} /> {/*내콘 화면 */}
-			<Route path='/home/giftcon-regi' element={<GiftConRegisterPage />} /> {/* 기프티콘 등록페이지 */}
 		</Routes>
 	);
 };
