35bdd5e9bb1d2f66a8e243ea11b3618d5b6838e7
Jo Seung Hyo <seunghyo7742@naver.com>
2023-12-03 19:27:17 +0900

    ✨ Feat: 지도 내 위치 추가

--- switch-con/src/lib/kakaoMap.js
+++ switch-con/src/lib/kakaoMap.js
@@ -2,12 +2,39 @@ import { useEffect } from 'react';
 
 const NearbyStoreMap = () => {
 	useEffect(() => {
-		const container = document.getElementById('map');
-		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3,
-		};
-		const map = new kakao.maps.Map(container, options);
+		// 사용자 좌표 얻어오기 & Map생성
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					const lat = position.coords.latitude;
+					const lng = position.coords.longitude;
+
+					console.log('users position', lat, lng);
+					const container = document.getElementById('map');
+
+					const options = {
+						center: new kakao.maps.LatLng(lat, lng),
+						level: 3, // 확대수준
+					};
+					const map = new kakao.maps.Map(container, options);
+					const circle = new kakao.maps.Circle({
+						center: new kakao.maps.LatLng(lat, lng),
+						radius: 20, //미터 단위의 원의 반지름
+						strokeWeight: 4, //선의 두께
+						strokeColor: '#75b8fa',
+						strokeOpacity: 0.8, //선의 불투명도 1에서 0사이, 0에 가까울수록 투명
+						fillColor: '#CFE7FF', //채우기 색
+						fillOpacity: 0.6,
+					});
+					circle.setMap(map);
+				},
+				(error) => {
+					console.error('사용자 위치정보 가져오는데 실패했습니다', error);
+				},
+			);
+		} else {
+			console.error('브라우저에서 geolocation 지원하지 않습니다.');
+		}
 	}, []);
 
 	return (
