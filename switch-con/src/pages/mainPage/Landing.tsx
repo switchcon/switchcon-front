3117309b979d86cf48cbac7863c805af21588b34
jeon-yuchan <69443437+jeon-yuchan@users.noreply.github.com>
2023-12-04 21:35:11 +0900

    merge

--- switch-con/src/pages/mainPage/Landing.tsx
+++ switch-con/src/pages/mainPage/Landing.tsx
@@ -24,6 +24,31 @@ const Landing = () => {
 		<div>
 			<Button>로딩중</Button>
 		</div>
+
+		/* <div>
+get
+<Button onClick={ExPost}>ExPost get test</Button>
+<Button onClick={ExReq}>ExReq get test</Button>
+<Button onClick={() => gifticonAll('expiringSoon')}>gifticonAll get test</Button>
+<Button onClick={() => gifticonDetail(29)}>gifiticonDetail get test</Button>
+<Button onClick={() => gifticonExAll('expiringSoon')}>gifiticonExALL get test</Button>
+<Button onClick={() => gifticonExDetail(9)}>gifiticonExDetail get test</Button>
+<Button onClick={userInfo}>userInfo get test</Button>
+post
+<Button onClick={() => gifticonExPost(40, '치킨')}>gifticonExPost post test</Button>
+<Button onClick={() => gifticonExReqPost(39, 9)}>gifticonExReqPost post test</Button>
+<Button onClick={() => gifticonExSuccessPost(7, 5)}>gifticonExSuccessPost post test</Button>
+<Button
+	onClick={() => gifticonPost('/images/image_url_2.jpg', '1', '1', '1', '1', '1', '2024-11-29', 10000, true)}
+>
+	gifticonPost post test
+</Button>
+<Button onClick={() => ocrPost('/images/image_url_2.jpg')}>ocr post test</Button>
+delete
+<Button onClick={() => gifticonDelete(30)}>gifticonDelete delete test</Button>
+<Button onClick={() => gifticonExDelete(7)}>gifticonExDelete delete test</Button>
+<Button onClick={() => gifticonExReqDelete(6, 6)}>gifticonExReqDelete delete test</Button>
+</div> */
 	);
 };
 
