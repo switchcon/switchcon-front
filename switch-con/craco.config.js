abe5c5311d7be0b8853d3ef6a7bdea5eb7726cf7
jeon-yuchan <69443437+jeon-yuchan@users.noreply.github.com>
2023-12-04 21:41:15 +0900

    merge

--- switch-con/craco.config.js
+++ switch-con/craco.config.js
@@ -6,6 +6,11 @@ module.exports = {
 			'@components': path.resolve(__dirname, 'src/components'),
 			'@pages': path.resolve(__dirname, 'src/pages'),
 			'@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@lib/*': path.resolve(__dirname, 'src/lib'),
+			'@apis/*': path.resolve(__dirname, 'src/apis'),
+			'@get/*': path.resolve(__dirname, 'src/get'),
+			'@post/*': path.resolve(__dirname, 'src/post'),
+			'@delelte/*': path.resolve(__dirname, 'src/delete'),
 		},
 	},
 };
