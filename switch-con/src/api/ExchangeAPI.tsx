51357d73059d2e884d830137f3cb56f83d83e80a
Jo Seung Hyo <seunghyo7742@naver.com>
2023-12-07 01:43:28 +0900

    ✨ Feat: 교환 홈 api연결

--- switch-con/src/api/ExchangeAPI.tsx
+++ switch-con/src/api/ExchangeAPI.tsx
@@ -8,11 +8,21 @@ const api = axios.create({
 		'Content-Type': 'application/json',
 	},
 });
+// 교환게시물 전체 조회
+export const getAllExchangePost = async (sortType) => {
+	//sortType: latest (최신등록순), expiringSoon(유효기간임박순), highPrice(높은 가격순), lowPrice(낮은가격순)
+	try {
+		const response = await api.get(`/exchange/all/${sortType}`);
+		return response.data.data;
+	} catch (error) {
+		console.error('Error during giftconAll', error);
+		throw error;
+	}
+};
 
 //exchange
 //교환 게시물 상세조회
 //교환게시물삭제
-// 교환게시물 전체 조회
 //교한 요청 등록
 //교환수락
 //교환 요청
